aecf277db8240ad67546ca78703fdef92cd9d48b
Marc Sportiello <mesportiello@gmail.com>
2024-07-08 16:20:34 +0200

    Change compass library code to QMC5883

--- firmware/main/compass.cpp
+++ firmware/main/compass.cpp
@@ -4,14 +4,14 @@
 #include <driver/i2c.h>
 #include <esp_log.h>
 
-#define I2C_ADDRESS 0x1e
+#define I2C_ADDRESS 0x0d
 
-#define REG_MODE 0x02
-#define REG_DATA_START 0x03
+#define REG_DATA_START 0x00
+#define REG_STATUS 0x06
+#define REG_CONTROL_1 0x09
+#define REG_RESET 0x0b
 
-#define MODE_CONTINUOUS 0x00
-#define MODE_SINGLE 0x01
-#define MODE_IDLE 0x03
+#define MODE_STANDBY 0x00
 
 static const char* TAG = "compass";
 
@@ -42,27 +42,41 @@ Compass::Compass(gpio_num_t sda, gpio_num_t scl) {
 
     i2c_param_config(I2C_NUM_0, &config);
     i2c_driver_install(I2C_NUM_0, config.mode, 0, 0, 0);
+
+    uint8_t data[2];
+
+    data[0] = REG_RESET;
+    data[1] = 0x01;
+    i2c_master_write_to_device(I2C_NUM_0, I2C_ADDRESS, data, 2, pdMS_TO_TICKS(50));
+
+    data[0] = REG_CONTROL_1;
+    data[1] = 0b01000000;
+    i2c_master_write_to_device(I2C_NUM_0, I2C_ADDRESS, data, 2, pdMS_TO_TICKS(50));
 }
 
 Vector3 Compass::read() {
     uint8_t data[6];
 
-    data[0] = REG_MODE;
-    data[1] = MODE_SINGLE;
+    bool ready = false;
+    while (!ready) {
+        data[0] = REG_STATUS;
+        i2c_master_write_to_device(I2C_NUM_0, I2C_ADDRESS, data, 1, pdMS_TO_TICKS(50));
 
-    i2c_master_write_to_device(I2C_NUM_0, I2C_ADDRESS, data, 2, pdMS_TO_TICKS(50));
+        i2c_master_read_from_device(I2C_NUM_0, I2C_ADDRESS, data, 1, pdMS_TO_TICKS(50));
 
-    vTaskDelay(pdMS_TO_TICKS(100));
+        ready = data[0] & 0x01;
+
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
 
     data[0] = REG_DATA_START;
-
     i2c_master_write_to_device(I2C_NUM_0, I2C_ADDRESS, data, 1, pdMS_TO_TICKS(50));
 
     i2c_master_read_from_device(I2C_NUM_0, I2C_ADDRESS, data, 6, pdMS_TO_TICKS(50));
 
-    int16_t x = data[0] << 8 | data[1];
-    int16_t z = data[2] << 8 | data[3];
-    int16_t y = data[4] << 8 | data[5];
+    int16_t x = data[1] << 8 | data[0];
+    int16_t y = data[3] << 8 | data[2];
+    int16_t z = data[5] << 8 | data[4];
 
     return Vector3(x / 2048.0f, y / 2048.0f, z / 2048.0f);
 }
